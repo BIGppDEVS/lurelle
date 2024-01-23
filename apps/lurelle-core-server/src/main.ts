41ca20bc38982e867ab08c45b50da35781ec9f67
Atharva Rane <atharvarane051102@gmail.com>
2024-01-23 10:52:32 +0530

    initialize nextjs tailwind web client

--- apps/lurelle-core-server/src/main.ts
+++ apps/lurelle-core-server/src/main.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 8080;
 
 const app = express();
 
@@ -10,5 +10,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
+  console.log(`[ server ] http://${host}:${port}`);
 });
---
+++ apps/lurelle-web-client/app/page.tsx
@@ -0,0 +1,7 @@
+export default async function Index() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-9xl m-6 font-semibold">👋 sup!</h1>
+    </div>
+  );
+}
